ce133ed50078e728d2ee06fa87601d3304026948
rakshithahj <rakshithahj48@gmail.com>
2026-06-05 22:14:26 +0530

    Update 3_Machine_Learning.py

--- pages/3_Machine_Learning.py
+++ pages/3_Machine_Learning.py
@@ -1,1 +1,36 @@
+import streamlit as st
 
+st.title(
+    "🤖 Machine Learning Models"
+)
+
+st.markdown(
+    """
+    Compare Multiple Algorithms
+    """
+)
+
+model = st.selectbox(
+
+    "Select Model",
+
+    [
+
+        "Linear Regression",
+
+        "KNN",
+
+        "Random Forest",
+
+        "Gradient Boosting"
+
+    ]
+)
+
+if st.button(
+    "Train Model"
+):
+
+    st.success(
+        f"{model} Trained Successfully"
+    )
